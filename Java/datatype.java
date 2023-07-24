f90d310065ee30df49e11aec71752251a2f987e2
Swayam <swayamborude8695@gmail.com>
2023-07-24 22:46:18 +0530

    variable

---
+++ Java/datatype.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class datatype {
+
+    public static void main(String args[]) {
+        
+        // for int DataType
+        // 1.int = datatype
+        // 2.num1 = variable name
+        int num1 = 2;
+        int num2 = 5;
+        int num3 = 8;
+        int sum = num1 + num2 + num3;
+        System.out.println("Total Number of sum is:- " + sum);
+        
+    }
+    
+}
