53a48844f11a38fcb7277197c242ed1cbcf731e6
Swayam <swayamborude8695@gmail.com>
2023-12-24 22:08:55 +0530

    aa

--- Java/datatype.java
+++ Java/datatype.java
@@ -1,18 +1,42 @@
-package Java;
+package java;
 
 public class datatype {
 
-    public static void main(String args[]) {
-        
-        // for int DataType
-        // 1.int = datatype
-        // 2.num1 = variable name
-        int num1 = 2;
-        int num2 = 5;
-        int num3 = 8;
-        int sum = num1 + num2 + num3;
-        System.out.println("Total Number of sum is:- " + sum);
-        
+    public static void main(String[] args) {
+         
+        //1.Boolean DataType (True or False)
+        boolean isJavafun = true;
+        int a = 10;
+        int b = 20;
+        System.out.println(isJavafun);
+        System.out.println(a>b);
+
+        //2.Byte DataType (-128 to 127)
+        Byte c = 127;
+        System.out.println(c);
+
+        //3.Char DataType
+        char d = 'S';
+        System.out.println(d);
+
+        //4.Short DataType (-32768 to 32767)
+        short f = 32767;
+        System.out.println(f);
+
+        //5.Int DataType (-2147483648 to 2147483647)
+        int t = 2147483647;
+        System.out.println(t);
+
+        //6.Long DataType (-9223372036854775808 to 9223372036854775807)
+        long y = 9223372036854775807L;
+        System.out.println(y);
+
+        //7.Float DataType (Upto 7 Decimal Digits)
+        float s = 12.1234567f;
+        System.out.println(s);
+
+        //8.Double DataType (Upto 16 Decimal Digits)
+        double q = 34.123456789123456;
+        System.out.println(q);
     }
-    
 }
